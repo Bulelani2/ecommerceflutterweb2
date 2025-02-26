aebe28c8fea0eee03382d178995a12e2a1e43af1
Bulelani Vincent Magwebu <114813817+Bulelani2@users.noreply.github.com>
2025-02-26 07:55:04 +0200

    Update keys.dart

--- lib/constants/keys.dart
+++ lib/constants/keys.dart
@@ -1,11 +1,11 @@
 String PublishableKey =
-    "pk_test_51QfdfKJeXUs6PxpasNsBou5gP4s1WQaBpNqYcajYjh0GzwZJqjjgbUmF4RmuxfTq8p8pSFBBOjdxDVb6lKvXGVrP00ObyRWzEj";
+    "";
 String SecretKey =
-    "sk_test_51QfdfKJeXUs6PxpaISskjjdKw6BpeytIi82KMvnQ2SMNZNNgdWk9A0OKJ6w0UogzbxoCfWA0eMXGXSOqnJ22suMf00vZ7XNhiy";
-String SupaBaseuri = "https://vbtozhccfcctuwrdwmza.supabase.co";
+    "";
+String SupaBaseuri = "";
 String SupaBaseanonKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZidG96aGNjZmNjdHV3cmR3bXphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NTc2NDQsImV4cCI6MjA1MjQzMzY0NH0.T_I6BR79V1BSHgIYTsofzDi8z_8q96eA1CSMCYg4NGU";
+    "";
 
-final ServiceId = "service_w5b0rsk";
-final TemplateId = "template_h0wjfbe";
-final UserId = "K-46QqLPMufyqJlkp";
+final ServiceId = "";
+final TemplateId = "";
+final UserId = "";
